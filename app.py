83afb6011860e1bbced00d5b98de25c549326e32
Felipe Oliveira <lipefan0@gmail.com>
2024-09-04 15:43:30 -0300

    Adding create user function with unique username validation

--- app.py
+++ app.py
@@ -44,6 +44,28 @@ def logout():
     logout_user()
     return jsonify({'message': 'Logout success'})
 
+
+@app.route('/user', methods=['POST'])
+# @login_required # Protege a criação de usuário para apenas usuários logados
+def create_user():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        if username and password:
+            user_filter = User(username=username, password=password)
+            db.session.add(user_filter)
+            db.session.commit()
+            return jsonify({'message': 'User created successfully'})
+        return jsonify({'message': 'Invalid username or password'}), 400
+    
+    return jsonify({'message': 'User already exists'}), 400
+    
+
+
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'

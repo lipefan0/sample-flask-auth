15f328136d6e3d74e75a21c6ddb6decf14729850
Felipe Oliveira <lipefan0@gmail.com>
2024-09-04 14:39:32 -0300

    Adding route and function to login

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
+
+# view login
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        # login
+        return jsonify({'message': 'Login success'}), 200
+    return jsonify({'message': 'Invalid username or password'}), 401
 
 @app.route('/', methods=['GET'])
 def hello_world():

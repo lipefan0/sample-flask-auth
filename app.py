322c3ceae6bfca697724703fe75baa6bdcb46743
Felipe Oliveira <lipefan0@gmail.com>
2024-09-04 15:21:02 -0300

    Adding logout function with login required

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -36,6 +36,13 @@ def login():
         return jsonify({'message': 'Invalid username or password'}), 400
     
     return jsonify({'message': 'Invalid username or password'}), 400
+
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logout success'})
 
 @app.route('/', methods=['GET'])
 def hello_world():

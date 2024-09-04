da360e24c29c8beb15c5746c9c68a2caef1c54b5
Felipe Oliveira <lipefan0@gmail.com>
2024-09-04 15:12:20 -0300

    Updating login function to get the user from db and recover it by cookie

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -13,7 +13,11 @@ db.init_app(app)
 login_manager.init_app(app)
 
 # view login
+login_manager.login_view = 'login'
 
+@login_manager.user_loader # recuperar usuario
+def load_user(user_id):
+    return User.query.get(user_id)
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -23,8 +27,15 @@ def login():
 
     if username and password:
         # login
-        return jsonify({'message': 'Login success'}), 200
-    return jsonify({'message': 'Invalid username or password'}), 401
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({'message': 'Login success'})
+        return jsonify({'message': 'Invalid username or password'}), 400
+    
+    return jsonify({'message': 'Invalid username or password'}), 400
 
 @app.route('/', methods=['GET'])
 def hello_world():
